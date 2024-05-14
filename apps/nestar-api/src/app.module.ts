04c37f343cbecfd569a8c08dcf9a904e70af2580
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-05-14 18:11:25 +0900

    feat:develop Erroring

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -7,20 +7,37 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
+import { T } from './libs/types/common';
 
 @Module({
 	imports: [
+		// boshqa module lar import orqali kirib keladi va export qilamiz
+		// ConfigModule = .env ni ichidagi datalarni olib beryapti
 		ConfigModule.forRoot(),
+		// By default bizni server REST API va biz uni GraphQl ga GraphQL module orqali ozgartiryapmiz
 		GraphQLModule.forRoot({
+			//ApolloDriver like adapter
 			driver: ApolloDriver,
+			// playground = for testing purposes
 			playground: true,
 			uploads: false,
+			// if enabled, GraphQL schema will be generated automatically
 			autoSchemaFile: true,
+			formatError: (error: T) => {
+				const graphQLFormattedError = {
+					code: error?.extensions.code,
+					message:
+						error?.extensions.exception?.response.message || error?.extensions?.response?.message || error?.message,
+				};
+				console.log('GRAPHQL GLOBAL ERROR:', graphQLFormattedError);
+				return graphQLFormattedError;
+			},
 		}),
-		ComponentsModule,
-		DatabaseModule,
+		ComponentsModule, // HTTP orqali
+		DatabaseModule, // TCP orqali
+		//TODO: Socket Connection
 	],
-	controllers: [AppController],
+	controllers: [AppController], //HTTP
 	providers: [AppService, AppResolver],
 })
 export class AppModule {}
